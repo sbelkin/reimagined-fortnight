5787413d5b4aba0ada23204c5b0afbd12c6683fd
sbelkin <sabelkin@gmail.com>
2016-05-22 22:54:50 -0400

    using a actorOf system instead of producerTemplate

--- broker/src/main/scala/io/elixir/broker/producer/MessageProducer.scala
+++ broker/src/main/scala/io/elixir/broker/producer/MessageProducer.scala
@@ -3,17 +3,21 @@ package io.elixir.broker.producer
 /**
   * Created by sbelkin on 5/22/2016.
   */
+import akka.pattern.ask
 
-import akka.actor.ActorSystem
-import akka.camel.CamelExtension
+import akka.actor.{Actor, ActorSystem, Props}
+import akka.camel.{CamelExtension, CamelMessage, Producer}
 import io.elixir.broker.consumer.route.QueueNames
+
+class ProducerSender(uri: String) extends Actor with Producer {
+  def endpointUri = uri
+}
 
 object MessageProducer extends App {
 
   val system = ActorSystem("some-system")
-  val camel = CamelExtension(system)
-  val camelContext = camel.context
-  val producerTemplate = camel.template
-  producerTemplate.sendBody(QueueNames.fooBar, "Message to Foo Bar")
+  val producer = system.actorOf(Props(classOf[ProducerSender], QueueNames.fooBar))
+
+  producer ! CamelMessage("bar", Map(CamelMessage.MessageExchangeId -> "123"))
 
 }
